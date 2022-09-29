e4e556af9c70026c0e4d22d60e7592687741cb95
naksha3 <114646116+naksha3@users.noreply.github.com>
2022-09-29 14:45:20 +0530

    Create KEIL.c

---
+++ KEIL.c
@@ -0,0 +1,18 @@
+#include<lpc21xx.h>
+main()
+{
+int a=6,b=2,sum,sub,mul,div;
+sum=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+}
+#include<lpc21xx.h>
+main()
+{
+int a=3, b=7, and, or, xor,not;
+  and=a&b;
+or=a|b;
+xor=a^b;
+not=~a;
+}
