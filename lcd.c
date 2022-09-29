447bcea8d5ed5ac0e55236c9f7f450b6943139e4
naksha3 <114646116+naksha3@users.noreply.github.com>
2022-09-29 14:40:40 +0530

    Update lcd.c

--- lcd.c
+++ lcd.c
@@ -1,5 +1,5 @@
 #include<lpc214x.h>
-#include<stdio.h> //Function prototypes void lcd_init(void);
+#include<stdio.h>
 void wr_cn(void);
 void clr_disp(void);
 void delay(unsigned int);
@@ -11,108 +11,106 @@ unsigned long int temp,r=0;
 unsigned char *ptr,disp[] = "SVIT BENGALURU",disp1[] = "LCD INTERFACING";
 int main()
 {
-PINSEL0 = 0X00000000; // configure P0.0 TO P0.15 as GPIO
-IO0DIR = 0x000000FC; //configure o/p lines for lcd [P0.2-P0.7]
-lcd_init(); //lcd intialisation
-delay(3200); // delay 1.06ms
-clr_disp(); //clear display
-delay(3200); // delay 1.06ms
-temp1 = 0x81; //Display starting address of first line 2nd pos
-lcd_com(); //function to send command to lcd
-ptr = disp; // pointing data
+PINSEL0 = 0X00000000; 
+IO0DIR = 0x000000FC;
+lcd_init(); 
+delay(3200); 
+clr_disp(); 
+delay(3200);
+temp1 = 0x81;
+lcd_com(); 
+ptr = disp; 
 while(*ptr!='\0')
 {
 temp1 = *ptr;
-lcd_data(); //function to send data to lcd
+lcd_data(); 
 ptr ++;
 }
-temp1 = 0xC0; // Display starting address of second line 1st pos
-lcd_com(); //function to send command to lcd
-ptr = disp1; // pointing second data
+temp1 = 0xC0;
+lcd_com(); 
+ptr = disp1; 
 while(*ptr!='\0')
 {
 temp1 = *ptr;
-lcd_data(); //send data to lcd
+lcd_data();
 ptr ++;
 }
 while(1);
-} //end of main()
-// lcd initialisation routine.
+} 
 void lcd_init(void)
 {
-temp = 0x30; //command to test LCD voltage level
+temp = 0x30; 
 wr_cn();
 delay(3200);
-temp = 0x30; //command to test LCD voltage level
+temp = 0x30; 
 wr_cn();
 delay(3200);
-temp = 0x30; //command to test LCD voltage level
+temp = 0x30; 
 wr_cn();
 delay(3200);
-temp = 0x20; // change to 4 bit mode from default 8 bit mode
+temp = 0x20;
 wr_cn();
 delay(3200);
-temp1 = 0x28; // load command for lcd function setting with lcd in 4 bit mode,
-lcd_com(); // 2 line and 5x7 matrix display
+temp1 = 0x28;
+lcd_com(); 
 delay(3200);
-temp1 = 0x0C; // load a command for display on, cursor on and blinking off
+temp1 = 0x0C; 
 lcd_com();
 delay(800);
-temp1 = 0x06; // command for cursor increment after data dump
+temp1 = 0x06; 
 lcd_com();
 delay(800);
-temp1 = 0x80; // set the cursor to beginning of line 1
+temp1 = 0x80;
 lcd_com();
 delay(800);
 }
 void lcd_com(void)
 {
-temp = temp1 & 0xf0; //masking higher nibble first
+temp = temp1 & 0xf0; 
 wr_cn();
-temp = temp1 & 0x0f; //masking lower nibble
+temp = temp1 & 0x0f; 
 temp = temp << 4;
 wr_cn();
-delay(500); // some delay
+delay(500); 
 }
-// command nibble o/p routine
-void wr_cn(void) //write command reg
+
+void wr_cn(void) 
 {
-IO0CLR = 0x000000FC; // clear the port lines.
-IO0SET= temp; // Assign the value to the PORT lines
-IO0CLR = 0x00000004; // clear bit RS = 0
-IO0SET= 0x00000008; // E=1
+IO0CLR = 0x000000FC; 
+IO0SET= temp;
+IO0CLR = 0x00000004; 
+IO0SET= 0x00000008; 
 delay(10);
-IO0CLR = 0x00000008; //E=0
+IO0CLR = 0x00000008; 
 }
-// data nibble o/p routine
-void wr_dn(void) // write data reg
+
+void wr_dn(void) 
 {
-IO0CLR = 0x000000FC; // clear the port lines.
-IO0SET = temp; // Assign the value to the PORT lines
-IO0SET = 0x00000004; // set bit RS = 1
-IO0SET = 0x00000008; // E=1
+IO0CLR = 0x000000FC;
+IO0SET = temp; 
+IO0SET = 0x00000004; 
+IO0SET = 0x00000008; 
 delay(10);
-IO0CLR = 0x00000008; //E=0
+IO0CLR = 0x00000008; 
 }
-// data o/p routine which also outputs high nibble first
-// and lower nibble next
+
 void lcd_data(void)
 {
-temp = temp1 & 0xf0; //masking higher nibble first
+temp = temp1 & 0xf0; 
 temp = temp ;
 wr_dn();
-temp= temp1 & 0x0f; //masking lower nibble
-temp= temp << 4; //shift 4bit to left
+temp= temp1 & 0x0f;
+temp= temp << 4; 
 wr_dn();
 delay(100);
 }
-void clr_disp(void) // function to clear the LCD screen
+void clr_disp(void) 
 {
 temp1 = 0x01;
 lcd_com();
 delay(500);
 }
-void delay(unsigned int r1) // delay function using for loop
+void delay(unsigned int r1) 
 {
 for(r=0;r<r1;r++);
 }
