a84cc1bb52c678f36a00168a9e4d64019a17b1ce
naksha3 <114646116+naksha3@users.noreply.github.com>
2022-09-29 16:18:48 +0530

    Update steppermotor.c

--- steppermotor.c
+++ steppermotor.c
@@ -5,40 +5,40 @@ unsigned int var1 ;
 unsigned long int i = 0 , j = 0 , k = 0 ;
 int main(void)
 {
-PINSEL2 = 0x00000000; //P1.20 to P1.23 GPIO
-IO1DIR |= 0x00F00000 ; //P1.20 to P1.23 made as output
+PINSEL2 = 0x00000000; 
+IO1DIR |= 0x00F00000 ; 
 while(1)
 {
-for( j = 0 ; j < 50 ; j++ ) // 50 times in Clock wise Rotation
-clock_wise() ; // rotate one round clockwise
-for( k = 0 ; k < 65000 ; k++ ) ; // Delay to show anti_clock Rotation
-for( j=0 ; j < 50 ; j++ ) // 50 times in Anti Clock wise Rotation
-anti_clock_wise() ; // rotate one round anticlockwise
-for( k = 0 ; k < 65000 ; k++ ) ; // Delay to show ANTI_clock Rotation
+for( j = 0 ; j < 50 ; j++ ) 
+clock_wise() ; 
+for( k = 0 ; k < 65000 ; k++ ) ; 
+for( j=0 ; j < 50 ; j++ ) 
+anti_clock_wise() ; 
+for( k = 0 ; k < 65000 ; k++ ) ; 
 }
-} // End of main
+} 
 void clock_wise(void)
 {
-var1 = 0x00080000; //For Clockwise
-for( i = 0 ; i <= 3 ; i++ ) // for A B C D Stepping
+var1 = 0x00080000;
+for( i = 0 ; i <= 3 ; i++ ) 
 {
 var1 <<= 1 ;
-IO1CLR =0x00F00000 ; //clearing all 4 bits
-IO1SET = var1 ; // setting particular bit
-for( k = 0 ; k < 3000 ; k++ ); //for step speed variation
+IO1CLR =0x00F00000 ;
+IO1SET = var1 ;
+for( k = 0 ; k < 3000 ; k++ );
 }
 }
 void anti_clock_wise(void)
 {
-var1 = 0x00800000 ; //For Anticlockwise
-IO1CLR =0x00F00000 ; //clearing all 4 bits
+var1 = 0x00800000 ; 
+IO1CLR =0x00F00000 ; 
 IO1SET = var1 ;
 for( k = 0 ; k < 3000 ; k++ ) ;
-for( i = 0 ; i < 3 ; i++ ) // for A B C D Stepping
+for( i = 0 ; i < 3 ; i++ ) 
 {
-var1 >>=1; //rotating bits
-IO1CLR =0x00F00000 ; // clear all bits before setting
-IO1SET = var1 ; // setting particular bit
-for( k = 0 ; k < 3000 ; k++ ) ; //for step speed variation
+var1 >>=1; 
+IO1CLR =0x00F00000 ; 
+IO1SET = var1 ;
+for( k = 0 ; k < 3000 ; k++ ) ; 
 }
 }
