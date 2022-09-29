3bd1c9919dc6446cd392f87cee4babac532f07ab
naksha3 <114646116+naksha3@users.noreply.github.com>
2022-09-29 14:42:49 +0530

    Update motor.asm

--- motor.asm
+++ motor.asm
@@ -4,27 +4,27 @@ PA EQU 0D800H
  CR EQU 0D803H
 .CODE
  MOV AX, @DATA ;
- MOV DS, AX ; DATA segment Initialization
- MOV AL, 80H ; Port A, B & C - Output
+ MOV DS, AX ; 
+ MOV AL, 80H ; 
  MOV DX, CR ;
- OUT DX, AL ; Initialize 8255
- MOV CX, 10 ; Set LOOP counter to repeat clk times
+ OUT DX, AL ; 
+ MOV CX, 10 ;
  MOV DX, PA
- MOV AL, 11H ; Store bit Pattern IN AL
+ MOV AL, 11H ; 
  L1: OUT DX, AL
  CALL DELAY
- ROR AL, 1 ; Rotate AL right by one bit to get next Pattern
+ ROR AL, 1 ; 
  LOOP L1
  MOV AH, 4CH ;
- INT 21H ; Exit to DOS
+ INT 21H ;
 DELAY PROC
  PUSH CX
  PUSH BX
  MOV CX, 1000h
 LOOP2:MOV BX, 0FFFFH
 LOOP1:DEC BX
- JNZ LOOP1 ; Repeat INNER LOOP FFFFH ×1000H times
- LOOP LOOP2 ; Repeat OUTER LOOP FFFFH times
+ JNZ LOOP1 ; 
+ LOOP LOOP2 ; 
  POP BX
  POP CX
  RET
